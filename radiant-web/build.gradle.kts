6639264f4dcdce7e20dabebba8e3c973b557e46f
mooowu <me@mooowu.xyz>
2025-09-23 12:33:45 +0900

    chore: OAuth2 의존성 추가 및 설정 정보 추가

--- radiant-web/build.gradle.kts
+++ radiant-web/build.gradle.kts
@@ -13,9 +13,16 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
     implementation("com.graphql-java:graphql-java-extended-scalars")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.test:spring-test")
